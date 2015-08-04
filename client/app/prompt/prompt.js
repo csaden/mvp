b9cef53a9ca63934eac33d5e336671182f5e21f4
Chris Saden <csaden@gmail.com>
2015-08-04 03:05:14 -0700

    fixed asset link and added responses array to controller

--- client/app/prompt/prompt.js
+++ client/app/prompt/prompt.js
@@ -1,18 +1,33 @@
 angular.module('writelet.prompt', [])
 
-.controller('PromptController', function PromptController($scope, $location, Prompts) {
+.controller('PromptController', function PromptController($scope, $location, Prompt) {
   $scope.prompt = {};
+  $scope.responses = [];
   $scope.loading = false;
 
   $scope.addPrompt = function () {
     $scope.loading = true;
-    Prompts.addPrompt($scope.prompt)
+    Prompt.addPrompt($scope.prompt)
       .then(function(prompt) {
         $scope.loading = false;
-        $location.path('/');
+        $scope.prompt = prompt;
+        $location.path('/prompt/' + prompt.id);
+        console.log('successfully received prompt: ', prompt.id);
       })
       .catch(function(error) {
         console.log(error);
+      });
+  };
+
+  $scope.getResponses = function () {
+    $scope.loading = true;
+    Prompt.getResponses($scope.prompt)
+      .then(function(responses) {
+        $scope.loading = false;
+        console.dir(responses);
+        $scope.responses = responses;
+        console.log('successfully received responses: ', responses.length);
+
       });
   };
 });
